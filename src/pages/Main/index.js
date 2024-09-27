3bb55b94caab7891cc5b254d4a4a8a82ce13bc05
JeuVictor <jeu.victor14@gmail.com>
2024-09-27 10:41:23 -0300

    Animações da pagina home

---
+++ src/pages/Main/index.js
@@ -0,0 +1,70 @@
+import React, {useCallback, useState} from "react"
+import {FaGithub, FaPlus, FaSpinner} from 'react-icons/fa'
+import { Container, Form, SubmitButton } from "./style"
+import api from "../../services/api";
+
+
+export default function Main(){
+    
+    const [newRepo, setnewRepo] = useState(''); 
+    const [repositorios, setRepositorios] = useState([]);
+    const [loading, setLoading] = useState(false)
+    
+    const handleSubmit = useCallback ((e)=>{
+        e.preventDefault();
+        async function submit(){
+            setLoading(true);
+            try{
+                const response = await api.get(`repos/${newRepo}`);
+            
+                const data = {
+                    name: response.data.full_name,
+                }
+           
+                setRepositorios([...repositorios, data]);
+                setnewRepo('');
+
+            }catch(error){
+                console.log(error)
+            }finally{
+                setLoading(false);
+            }
+        }
+
+        submit();
+
+     }, [newRepo, repositorios]);
+        
+    
+    function handleinputChange(e){
+        setnewRepo(e.target.value);
+    }
+    return (
+        <Container>
+            <h1>
+                <FaGithub size={25}/>
+                Menu
+            </h1>
+
+            <Form onSubmit={handleSubmit}>
+                <input 
+                type="text" 
+                placeholder="Adicionar Repositorios"
+                value={newRepo}
+                onChange={handleinputChange} />
+                
+                <SubmitButton loading={loading ? 1:0}>
+                    {
+                        loading ? (
+                            <FaSpinner color="#fff" size={14}/>
+                        ): (
+                            <FaPlus color=" #fff" size={14}/>
+                        )
+                    }
+                    
+                </SubmitButton>
+            </Form>
+            
+        </Container>
+    )
+}
